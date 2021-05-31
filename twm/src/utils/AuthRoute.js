68f8a497bc2970aedba9e259c20128e027d19ca2
Julian Graves <Juliangraves96@gmail.com>
2021-05-31 09:49:08 -0700

    w

--- twm/src/utils/AuthRoute.js
+++ twm/src/utils/AuthRoute.js
@@ -4,19 +4,6 @@ import { Route, Redirect } from 'react-router-dom';
 import { AuthContext } from '../context/auth';
 
 function AuthRoute({ component: Component, ...rest }) {
-<<<<<<< HEAD
-    const { user } = useContext(AuthContext);
-
-    return (
-        <Route 
-            {...rest}
-            render={(props) => (user ? <Redirect to="/" /> : <Component {...props} />)}
-        />
-    )
-}
-
-export default AuthRoute;
-=======
   const { user } = useContext(AuthContext);
 
   return (
@@ -28,4 +15,3 @@ export default AuthRoute;
 }
 
 export default AuthRoute;
->>>>>>> a1e717675c4a5932f013cbcd6a0dde869c3a51b5
