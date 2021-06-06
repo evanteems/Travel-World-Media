bbe0b6d1a3d21efbde1bf18917c6fc14a344f6b6
caleb <calebp80@gmail.com>
2021-06-06 13:21:47 -0700

    // eslint-disable-next-line react/prop-type

--- client/src/util/Tooltip.js
+++ client/src/util/Tooltip.js
@@ -1,8 +1,9 @@
 import { Popup } from 'semantic-ui-react';
 import React from 'react';
 
+
 function Tooltip({ content, children }) {
-    return <Popup inverted content={content} trigger={children} />;
+  return <Popup inverted content={content} trigger={children} />;
 }
 
 export default Tooltip;
