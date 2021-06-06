7f6c9bb52c361d43fb18ec8d65de216d9c4c7db9
caleb <calebp80@gmail.com>
2021-06-06 13:13:03 -0700

    reducers

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -14,4 +14,28 @@ if (localStorage.getItem('jwtToken')) {
     initialState.user = decodedToken;
   }
 }
+const AuthContext = createContext({
+  user: null,
+  login: (userData) => {},
+  logout: () => {},
+});
+
+// TODO: Learn more about reducers
+// A reducer basically receives an action with a type and a payload, then determines what to do based on the functionality
+function authReducer(state, action) {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case 'LOGOUT':
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
+}
 
